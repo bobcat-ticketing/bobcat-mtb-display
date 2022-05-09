f05ae80fc5a35ed54437fd6d2be7734883eac821
Jakob Schlyter <jakob@kirei.se>
2022-05-09 11:46:46 +0200

    replace treepoem

--- mtb_display.py
+++ mtb_display.py
@@ -2,15 +2,13 @@
 
 import argparse
 import asyncio
-import io
 import logging
 import time
 import zlib
 
 import pygame
-import treepoem
 from asyncio_mqtt import Client
-from PIL import EpsImagePlugin
+from aztec_code_generator import AztecCode
 
 DEFAULT_MQTT_HOSTNAME = "127.0.0.1"
 DEFAULT_MQTT_TOPIC = "service/v1/Validate/reader"
@@ -18,13 +16,19 @@ DEFAULT_MQTT_TOPIC = "service/v1/Validate/reader"
 RESOLUTION = WIDTH, HEIGHT = 480, 320
 COLOUR_WHITE = 255, 255, 255
 
+DEFAULT_AZTEC_SIZE = 2
 
-def mtb_display(screen, data, delay: float = 5.0) -> None:
+
+def mtb_display(
+    screen, data, delay: float = 5.0, size: int = DEFAULT_AZTEC_SIZE
+) -> None:
     """Show MTB"""
     logging.info("Display %d bytes for %.1f seconds", len(data), delay)
     barcode_data = zlib.compress(data)
-    image = generate_barcode(barcode_type="azteccode", data=barcode_data, options={})
-    ticket_image = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
+    image = AztecCode(data=barcode_data).image(module_size=size).convert(mode="RGB")
+    ticket_image = pygame.image.fromstring(
+        image.tobytes(), image.size, image.mode
+    ).convert()
     screen.blit(
         ticket_image, ((WIDTH - image.size[0]) / 2, (HEIGHT - image.size[1]) / 2)
     )
@@ -38,33 +42,14 @@ def mtb_display(screen, data, delay: float = 5.0) -> None:
     pygame.event.pump()
 
 
-def _format_code(barcode_type, data, options):
-    """From treepoem 1.0.1, convert to Py3.5+"""
-    return "<{data}> <{options}> <{barcode_type}> cvn".format(
-        data=data.hex(),
-        options=treepoem._format_options(options).encode().hex(),
-        barcode_type=barcode_type.encode().hex(),
-    )
-
-
-def generate_barcode(barcode_type, data, options):
-    """From treepoem 1.0.1, convert to Py3.5+"""
-    code = _format_code(barcode_type, data, options)
-    bbox_lines = treepoem._get_bbox(code)
-    full_code = treepoem.EPS_TEMPLATE.format(
-        bbox=bbox_lines, bwipp=treepoem.BWIPP, code=code
-    )
-    return EpsImagePlugin.EpsImageFile(io.BytesIO(full_code.encode("utf8")))
-
-
-async def receive_mqtt(client, topic, screen, delay):
+async def receive_mqtt(client, topic, screen, delay, size):
     """MQTT message receive loop"""
     await client.connect()
     await client.subscribe(topic)
     async with client.unfiltered_messages() as messages:
         async for message in messages:
             logging.debug("Got MQTT topic %s", message.topic)
-            mtb_display(screen, message.payload, delay)
+            mtb_display(screen, message.payload, delay, size)
 
 
 def main():
@@ -89,6 +74,9 @@ def main():
         "--topic", dest="topic", help="MQTT topic", default=DEFAULT_MQTT_TOPIC
     )
     parser.add_argument(
+        "--size", dest="size", help="AZTEC size", type=int, default=DEFAULT_AZTEC_SIZE
+    )
+    parser.add_argument(
         "--debug", dest="debug", action="store_true", help="Enable debugging"
     )
 
@@ -107,12 +95,14 @@ def main():
     for input_filename in args.mtb:
         with open(input_filename, "rb") as input_file:
             mtbdata = input_file.read()
-            mtb_display(screen, mtbdata, args.delay)
+            mtb_display(screen, mtbdata, args.delay, args.size)
 
     if args.mqtt:
         client = Client(hostname=args.hostname)
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(receive_mqtt(client, args.topic, screen, args.delay))
+        loop.run_until_complete(
+            receive_mqtt(client, args.topic, screen, args.delay, args.size)
+        )
         loop.stop()
 
 
